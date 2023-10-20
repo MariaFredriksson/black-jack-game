b1ed95de2b016eec275cd958aa1a13286d07788a
Maddelen Hedenstrom <mh222vu@student.lnu.se>
2023-10-20 16:05:41 +0200

    Add more tests

--- app/src/test/java/model/SimpleTest.java
+++ app/src/test/java/model/SimpleTest.java
@@ -15,8 +15,6 @@ import model.rules.RulesFactory;
 
 public class SimpleTest {
 
-
-  //^^Get this test to work!
  // Make a test to check for soft 17
   @Test
   public void testSoft17() {
@@ -35,8 +33,74 @@ public class SimpleTest {
 
     dealer.showHand();
 
-    // assertEquals(17, dealer.calcScore());
+    assertEquals(17, dealer.calcScore());
     assertEquals(true, dealer.hit(dealer));
+
+  }
+
+  @Test
+  public void test16() {
+    Dealer dealer = new Dealer(new RulesFactory());
+    
+    dealer.newGame(dealer);
+    dealer.clearHand();
+
+    Card.Mutable ace = new Card.Mutable(Card.Color.Diamonds, Card.Value.Ace);
+    Card.Mutable three = new Card.Mutable(Card.Color.Diamonds, Card.Value.Three);
+    Card.Mutable two = new Card.Mutable(Card.Color.Diamonds, Card.Value.Two);
+
+    dealer.dealCard(ace);
+    dealer.dealCard(three);
+    dealer.dealCard(two);
+
+    dealer.showHand();
+
+    assertEquals(16, dealer.calcScore());
+    assertEquals(true, dealer.hit(dealer));
+
+  }
+
+  @Test
+  public void testNotSoft17() {
+    Dealer dealer = new Dealer(new RulesFactory());
+    
+    dealer.newGame(dealer);
+    dealer.clearHand();
+
+    Card.Mutable king = new Card.Mutable(Card.Color.Diamonds, Card.Value.King);
+    Card.Mutable four = new Card.Mutable(Card.Color.Diamonds, Card.Value.Four);
+    Card.Mutable three = new Card.Mutable(Card.Color.Diamonds, Card.Value.Three);
+
+    dealer.dealCard(king);
+    dealer.dealCard(four);
+    dealer.dealCard(three);
+
+    dealer.showHand();
+
+    assertEquals(17, dealer.calcScore());
+    assertEquals(false, dealer.hit(dealer));
+
+  }
+
+  @Test
+  public void test18() {
+    Dealer dealer = new Dealer(new RulesFactory());
+    
+    dealer.newGame(dealer);
+    dealer.clearHand();
+
+    Card.Mutable king = new Card.Mutable(Card.Color.Diamonds, Card.Value.King);
+    Card.Mutable five = new Card.Mutable(Card.Color.Diamonds, Card.Value.Five);
+    Card.Mutable three = new Card.Mutable(Card.Color.Diamonds, Card.Value.Three);
+
+    dealer.dealCard(king);
+    dealer.dealCard(five);
+    dealer.dealCard(three);
+
+    dealer.showHand();
+
+    assertEquals(18, dealer.calcScore());
+    assertEquals(false, dealer.hit(dealer));
 
   }
 
