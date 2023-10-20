e983d629690cc3e77e199e1bb2cd685dcb13b764
Maddelen Hedenstrom <mh222vu@student.lnu.se>
2023-10-20 14:35:22 +0200

    Add test

--- app/src/test/java/model/SimpleTest.java
+++ app/src/test/java/model/SimpleTest.java
@@ -20,12 +20,8 @@ public class SimpleTest {
  // Make a test to check for soft 17
   @Test
   public void testSoft17() {
-    Player player = new Player();
     Dealer dealer = new Dealer(new RulesFactory());
-
-    dealer.newGame(player);
-
-    dealer.hit(player);
+    
     Card.Mutable ace = new Card.Mutable(Card.Color.Diamonds, Card.Value.Ace);
     Card.Mutable four = new Card.Mutable(Card.Color.Diamonds, Card.Value.Four);
     Card.Mutable two = new Card.Mutable(Card.Color.Diamonds, Card.Value.Two);
@@ -34,24 +30,10 @@ public class SimpleTest {
     dealer.dealCard(four);
     dealer.dealCard(two);
 
+    dealer.showHand();
+
     assertEquals(17, dealer.calcScore());
 
   }
 
-//   public class AlbumGeneratorTest {
-//    @Test
-//   public void testAlbumGenerator() {
-//     // Tests that the constructor and getters works correctly.
-//     AlbumGenerator albumGenerator = new AlbumGenerator();
-//     Album[] greatestAlbums = albumGenerator.generateAlbums();
-//     assertEquals(4, greatestAlbums.length);
-
-//     for (Album a : greatestAlbums) {
-//       assertNotNull(a);
-//       assertNotEquals("No name", a.getName());
-//       assertNotEquals("No artist", a.getArtist());
-//       assertNotEquals(-1, a.getYear());
-//     }
-//   }
-// }
 }
